485ac220bdb862977d1782a72243f49af6455d70
akrfb6 <45079204+akrfb6@users.noreply.github.com>
2021-08-03 10:09:43 +0530

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -7,9 +7,9 @@ const farms: FarmConfig[] = [
    */
   {
     pid: 0,
-    lpSymbol: 'CAKE',
+    lpSymbol: 'LEON',
     lpAddresses: {
-      97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
+      97: '0x2CdD3aB9cAed3669F30b09db69fC797361F33476',
       56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
     },
     token: tokens.syrup,
