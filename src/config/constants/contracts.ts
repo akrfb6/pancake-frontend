834e8832c032d47001719ab8949cb2199dbec3d5
akrfb6 <45079204+akrfb6@users.noreply.github.com>
2021-08-03 10:08:02 +0530

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,6 +1,6 @@
 export default {
   masterChef: {
-    97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
+    97: '0x28527e00b897C5F70B35e5897A79E1d69e8D6999',
     56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
   },
   sousChef: {
